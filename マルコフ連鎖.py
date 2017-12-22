78086206cdbf00eda70dd118683d18b92c9c21fd
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-12-22 10:25:52 +0900

    動いた?

--- マルコフ連鎖.py
+++ マルコフ連鎖.py
@@ -17,32 +17,23 @@ def add_rate(list):
 
 with open("res.txt",encoding="UTF-8") as f:
     for centence in f:
-        a=centence.split()
-        wordlist = ["<SOS>"] + a + ["<EOS>"]
-        #wordlist = ["<SOS>"] + list(map("".join,list(itertools.zip_longest(a[::2],a[1::2],fillvalue="")))) + ["<EOS>"]
-        #for left,right in zip(wordlist,wordlist[1:]):
-        add_rate(zip()
+        centence_list=centence.split()
+        wordlist = ["<SOS>"] + centence_list + ["<EOS>"]
+        add_rate([("<SOS>",wordlist[1])])
         add_rate(zip(map("".join,zip(wordlist,wordlist[1:])),wordlist[2:]))
-        #wordlist = list(map("".join,list(itertools.zip_longest(a[1::2],a[2::2],fillvalue="")))) + ["<EOS>"]
-        #for left,right in zip(wordlist,wordlist[1:]):
-        #    if left in rate:
-        #        if right in rate[left]:
-        #            rate[left].update({right:rate[left][right]+1})
-        #        else:
-        #            rate[left][right]=1
-        #    else:
-        #        rate[left]={right:1}
 
 print(rate)
+#['<SOS>', 'ちなみに', 'LOL', '入れ', 'てる', 'ん', '？', '<EOS>']
 
 import time
 while True:
-    sentence=""
-    word="<SOS>"
+    words=["<SOS>"]
+    i=-1
     while True:
-        word=random.choices(list(rate[word].keys()),list(rate[word].values()))[0]
+        word=random.choices(list(rate["".join(words[i:i+2])].keys()),list(rate["".join(words[i:i+2])].values()))[0]
         if word=="<EOS>":
             break
-        sentence+=word
-    print(sentence)
+        words.append(word)
+        i+=1
+    print("".join(words[1:]))
     time.sleep(2)
